dbb232ee73785747088020ba7c382e816f92e91a
mikufanliu <mikufanliu@gmail.com>
2019-04-27 08:28:53 +0800

    add all files

---
+++ src/Ebook.vue
@@ -0,0 +1,184 @@
+<template>
+<div class="ebook">
+    <title-bar :ifTitleAndMenuShow="ifTitleAndMenuShow"></title-bar>
+    <div class="read-wrapper">
+        <div id="read"></div>
+        <div class="mask">
+            <div class="left" @click="prevPage"></div>
+            <div class="center" @click="toggleTitleAndMenu"></div>
+            <div class="right" @click="nextPage"></div>
+        </div>
+    </div>
+    <menu-bar :ifTitleAndMenuShow="ifTitleAndMenuShow" :fontSizeList="fontSizeList" :defaultFontSize="defaultFontSize" @setFontSize="setFontSize"
+    :themeList="themeList"
+    :defaultTheme="defaultTheme"
+    @setTheme="setTheme"
+     ref="menuBar"></menu-bar>
+</div>
+</template>
+
+<script>
+import TitleBar from '@/components/TitleBar'
+import MenuBar from '@/components/MenuBar'
+import Epub from 'epubjs'
+const DOWNLOAD_URL = '/static/book1.epub'
+global.ePub = Epub
+export default {
+    components: {
+        TitleBar,
+        MenuBar
+    },
+    data() {
+        return {
+            ifTitleAndMenuShow: false,
+            fontSizeList: [{
+                    fontSize: 12
+                },
+                {
+                    fontSize: 14
+                },
+                {
+                    fontSize: 16
+                },
+                {
+                    fontSize: 18
+                },
+                {
+                    fontSize: 20
+                },
+                {
+                    fontSize: 22
+                },
+                {
+                    fontSize: 24
+                }
+            ],
+            defaultFontSize: 16,
+            themeList: [{
+                    name: 'default',
+                    style: {
+                        body: {
+                            'color': '#000',
+                            'background': '#fff'
+                        }
+                    }
+                },
+                {
+                    name: 'eye',
+                    style: {
+                        body: {
+                            'color': '#000',
+                            'background': '#ceeaba'
+                        }
+                    }
+                },
+                {
+                    name: 'night',
+                    style: {
+                        body: {
+                            'color': '#fff',
+                            'background': '#000'
+                        }
+                    }
+                },
+                {
+                    name: 'gold',
+                    style: {
+                        body: {
+                            'color': '#000',
+                            'background': 'rgba(241, 236, 226)'
+                        }
+                    }
+                }
+            ],
+            defaultTheme: 0
+        }
+    },
+    methods: {
+        setTheme(index) {
+         this.themes.select(this.themeList[index].name)
+         this.defaultTheme = index
+        },
+        registerTheme() {
+            this.themeList.forEach(theme => {
+                this.themes.register(theme.name, theme.style)
+            })
+        },
+        setFontSize(fontSize) {
+            this.defaultFontSize = fontSize
+            if (this.themes) {
+                this.themes.fontSize(fontSize + 'px')
+            }
+        },
+        toggleTitleAndMenu() {
+            this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+            if (!this.ifTitleAndMenuShow) {
+                this.$refs.menuBar.hideSetting()
+            }
+        },
+        prevPage() {
+            if (this.rendition) {
+                this.rendition.prev()
+            }
+        },
+        nextPage() {
+            if (this.rendition) {
+                this.rendition.next()
+            }
+        },
+        // 电子书的解析和渲染
+        showEpub() {
+            // 生成Rendition
+            this.book = new Epub(DOWNLOAD_URL)
+            this.rendition = this.book.renderTo(
+                'read', {
+                    width: window.innerWidth,
+                    height: window.innerHeight
+                })
+            this.rendition.display()
+            // 获取theme对象
+            this.themes = this.rendition.themes
+            // 默认字体设置
+            this.setFontSize(this.defaultFontSize)
+            // 主题设置
+            this.registerTheme()
+            this.setTheme(this.defaultTheme)
+        }
+    },
+    mounted() {
+        this.showEpub()
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/styles/global';
+
+.ebook {
+    position: relative;
+
+    .read-wrapper {
+        .mask {
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 100;
+            width: 100%;
+            height: 100%;
+            display: flex;
+
+            .left {
+                flex: 0 0 px2rem(100);
+            }
+
+            .center {
+                flex: 1;
+            }
+
+            .right {
+                flex: 0 0 px2rem(100);
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,226 @@
+<template>
+<div class="menu-bar">
+    <transition name="slide-up">
+        <div class="menu-wrapper" :class="{'hide-box-shadow':ifSettingShow || !ifTitleAndMenuShow}" v-show="ifTitleAndMenuShow">
+            <div class="icon-wrapper">
+                <span class="icon-menu icon"></span>
+            </div>
+            <div class="icon-wrapper">
+                <span class="icon-progress icon"></span>
+            </div>
+            <div class="icon-wrapper">
+                <span class="icon-bright icon" @click="showSetting(1)"></span>
+            </div>
+            <div class="icon-wrapper">
+                <span class="icon-a icon" @click="showSetting(0)">A</span>
+            </div>
+        </div>
+    </transition>
+    <transition name="slide-up">
+        <div class="setting-wrapper" v-show=" ifSettingShow">
+            <div class="setting-font-size" v-if="showTag === 0">
+                <div class="preview" :style="{fontSize: fontSizeList[0].fontSize + 'px'}">A</div>
+                <div class="select">
+                    <div class="select-wrapper" v-for="(item,index) in fontSizeList" :key="index" @click="setFontSize(item.fontSize)">
+                        <div class="line"></div>
+                        <div class="point-wrapper">
+                            <div class="point" v-show="defaultFontSize === item.fontSize">
+                                <div class="small-point"></div>
+                            </div>
+                        </div>
+                        <div class="line"></div>
+                    </div>
+                </div>
+                <div class="preview" :style="{fontSize: fontSizeList[fontSizeList.length - 1].fontSize + 'px'}">A</div>
+            </div>
+            <div class="setting-theme" v-else-if="showTag === 1">
+                <div class="setting-theme-item" v-for="(item,index) in themeList" :key="index" @click="setTheme(index)">
+                    <div class="preview" :style="{background: item.style.body.background}" :class="{'no-border': item.style.body.background !== '#fff'}"></div>
+                    <div class="text" :class="{'selected': index=== defaultTheme}">{{item.name}}</div>
+                </div>
+            </div>
+        </div>
+    </transition>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        ifTitleAndMenuShow: {
+            type: Boolean,
+            default: false
+        },
+        fontSizeList: Array,
+        defaultFontSize: Number,
+        themeList: Array,
+        defaultTheme: Number
+    },
+    data() {
+        return {
+            ifSettingShow: false,
+            showTag: 0
+        }
+    },
+    methods: {
+        setTheme(index) {
+            this.$emit('setTheme',index)
+        },
+        setFontSize(fontSize) {
+            this.$emit('setFontSize', fontSize)
+        },
+        showSetting(tag) {
+            this.ifSettingShow = true,
+            this.showTag = tag
+        },
+        hideSetting() {
+            this.ifSettingShow = false
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/global';
+
+.menu-bar {
+    .menu-wrapper {
+        &.hide-box-shadow {
+            box-shadow: none
+        }
+
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        z-index: 101;
+        display: flex;
+        width: 100%;
+        height: px2rem(48);
+        background: wheat;
+        box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+
+        .icon-wrapper {
+            flex: 1;
+            @include center;
+
+            .icon-progress {
+                font-size: px2rem(25);
+            }
+
+            .icon-bright {
+                font-size: px2rem(20);
+            }
+        }
+    }
+
+    .setting-wrapper {
+        position: absolute;
+        bottom: px2rem(48);
+        left: 0;
+        width: 100%;
+        height: px2rem(60);
+        z-index: 101;
+        background: rgb(252, 251, 251);
+        box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+
+        .setting-font-size {
+            display: flex;
+            height: 100%;
+
+            .preview {
+                flex: 0 0 px2rem(40);
+                @include center;
+
+            }
+
+            .select {
+                display: flex;
+                flex: 1;
+
+                .select-wrapper {
+                    flex: 1;
+                    display: flex;
+                    align-items: center;
+
+                    &:first-child {
+                        .line {
+                            &:first-child {
+                                border-top: none;
+                            }
+                        }
+                    }
+
+                    &:last-child {
+                        .line {
+                            &:last-child {
+                                border-top: none;
+                            }
+                        }
+                    }
+
+                    .line {
+                        flex: 1;
+                        height: 0;
+                        border-top: px2rem(1) solid #ccc;
+
+                    }
+
+                    .point-wrapper {
+                        position: relative;
+                        flex: 0 0 0;
+                        width: 0;
+                        height: px2rem(7);
+                        border-left: px2rem(1) solid #ccc;
+
+                        .point {
+                            position: absolute;
+                            top: px2rem(-8);
+                            left: px2rem(-10);
+                            width: px2rem(20);
+                            height: px2rem(20);
+                            border-radius: 50%;
+                            background: white;
+                            border: px2rem(1) solid #ccc;
+                            box-shadow: 0 px2rem(4) px2rem(4) rgba(0, 0, 0, .15);
+                            @include center;
+
+                            .small-point {
+                                width: px2rem(5);
+                                height: px2rem(5);
+                                border-radius: 50%;
+                                background: black;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .setting-theme {
+            height: 100%;
+            display: flex;
+            .setting-theme-item{
+                flex: 1;
+                display: flex;
+                flex-direction: column;
+                padding: px2rem(5);
+                box-sizing: border-box;
+                .preview {
+                    flex:1;
+                    border: px2rem(1) solid #ccc;
+                    box-sizing: border-box;
+                    &.no-border {
+                        border: none;
+                    }
+                }
+                .text {
+                    flex: 0 0 px2rem(20);
+                    font-size: px2rem(16);
+                    color:#ccc;
+                    @include center;
+                    &.selected {color: #333}
+                }
+            }
+        }
+    }
+}
+</style>
