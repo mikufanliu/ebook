0cd0850b61bc8c7c681058a0aaa6b45d18384b68
mikufanliu <mikufanliu@gmail.com>
2019-05-03 21:39:16 +0800

    setdown menuBar

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -9,13 +9,18 @@
             <div class="right" @click="nextPage"></div>
         </div>
     </div>
-    <menu-bar :ifTitleAndMenuShow="ifTitleAndMenuShow" :fontSizeList="fontSizeList" :defaultFontSize="defaultFontSize" @setFontSize="setFontSize"
-    :themeList="themeList"
-    :defaultTheme="defaultTheme"
-    @setTheme="setTheme"
-    :bookAvailable="bookAvailable"
-    @onProgressChange="onProgressChange"
-     ref="menuBar"></menu-bar>
+    <menu-bar  :ifTitleAndMenuShow="ifTitleAndMenuShow" 
+                            :fontSizeList="fontSizeList" :defaultFontSize="defaultFontSize" @setFontSize="setFontSize"
+                            :themeList="themeList"
+                            :defaultTheme="defaultTheme"
+                            @setTheme="setTheme"
+                            :bookAvailable="bookAvailable"
+                            @onProgressChange="onProgressChange"
+                            @jumpTo="jumpTo"
+                            :navigation="navigation"
+                            :bookProgress="bookProgress"
+                            ref="menuBar">
+     </menu-bar>
 </div>
 </template>
 
@@ -94,10 +99,31 @@ export default {
                 }
             ],
             defaultTheme: 0,
-            bookAvailable: false
+            bookAvailable: false,
+            navigation: {},
+            bookProgress: 0
         }
     },
     methods: {
+        showProgress() {
+            const currentLocation = this.rendition.currentLocation()
+            this.bookProgress = this.bookAvailable ? this.locations.percentageFromCfi(currentLocation.start.cfi):0
+            this.bookProgress = Math.round(this.bookProgress * 100)
+        },
+        //跳转到指定的链接
+        jumpTo(href) {
+            this.rendition.display(href).then(
+                () => {
+                    this.showProgress()
+                }
+            )
+            this.hideTitleAndMenu()
+        },
+        hideTitleAndMenu(){
+            this.ifTitleAndMenuShow = false
+            this.$refs.menuBar.hideSetting()
+            this.$refs.menuBar.hideContent()
+        },
         onProgressChange(progress) {
             const percentage = progress / 100
             const location = percentage > 0 ? this.locations.cfiFromPercentage(percentage) : 0
@@ -126,12 +152,21 @@ export default {
         },
         prevPage() {
             if (this.rendition) {
-                this.rendition.prev()
+                this.rendition.prev().then(
+                    () =>{
+                        this.showProgress()
+                    }
+                )
+
             }
         },
         nextPage() {
             if (this.rendition) {
-                this.rendition.next()
+                this.rendition.next().then(
+                    () =>{
+                        this.showProgress()
+                    }
+                )
             }
         },
         // 电子书的解析和渲染
@@ -140,8 +175,8 @@ export default {
             this.book = new Epub(DOWNLOAD_URL)
             this.rendition = this.book.renderTo(
                 'read', {
-                    width: window.innerWidth,
-                    height: window.innerHeight
+                    width: "100vw",
+                    height: "100vh"
                 })
             this.rendition.display()
             // 获取theme对象
@@ -153,15 +188,27 @@ export default {
             this.setTheme(this.defaultTheme)
             // 进度的获取
             this.book.ready.then(() => {
+                // 获取导航栏
+                this.navigation = this.book.navigation
                 return  this.book.locations.generate()
             }).then(() => {
                 this.locations = this.book.locations
                 this.bookAvailable = true
             })
+        },
+        handleScroll (e) {
+            //如果侧栏没有展示就监听鼠标滚轮事件
+            if(!this.$refs.menuBar.ifShowContent && (document.getElementsByClassName("mask").length == 1)){
+                e.deltaY>0 ? this.nextPage() : this.prevPage()}
         }
     },
     mounted() {
         this.showEpub()
+        // 监听鼠标滚轮事件
+        // ie and Chrome
+        window.addEventListener('mousewheel',this.handleScroll,false)
+        // firefox
+        window.addEventListener("DOMMouseScroll",this.handleScroll,false)
     }
 }
 </script>
@@ -171,7 +218,9 @@ export default {
 
 .ebook {
     position: relative;
-
+	// 隐藏侧边和底部的滚动条
+ 	overflow-y:hidden;
+  	overflow-x:hidden;
     .read-wrapper {
         .mask {
             position: absolute;
--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -3,7 +3,7 @@
     <transition name="slide-up">
         <div class="menu-wrapper" :class="{'hide-box-shadow':ifSettingShow || !ifTitleAndMenuShow}" v-show="ifTitleAndMenuShow">
             <div class="icon-wrapper">
-                <span class="icon-menu icon"></span>
+                <span class="icon-menu icon" @click="showNav()"></span>
             </div>
             <div class="icon-wrapper">
                 <span class="icon-progress icon" @click="showSetting(2)"></span>
@@ -39,29 +39,40 @@
                     <div class="text" :class="{'selected': index=== defaultTheme}">{{item.name}}</div>
                 </div>
             </div>
-            <div class="setting-progree" v-else-if="showTag === 2">
+            <div class="setting-progress" v-else-if="showTag === 2">
                 <div class="progress-wrapper">
                     <input type="range" class="progress"
                         max="100"
                         min="0"
                         step="1"
                         @change="onProgressChange($event.target.value)"
-                        @input="onProgressInput($event.target.value)"
-                        :value="progress"
+                        v-model="progress"
                         :disabled="!bookAvailable"
+                        :style="{backgroundSize: progress + '% 100%'}" 
                         ref="progress">
-                </div>
+                    </div>
                     <div class="text-wrapper">
                         <span>{{bookAvailable? progress + "%" : '加载中'}}</span>
                     </div>
                 </div>
             </div>
     </transition>
+    <content-view :ifShowContent="ifShowContent" v-show="ifShowContent" :navigation="navigation" :bookAvailable="bookAvailable" @jumpTo="jumpTo">
+    </content-view>
+    <transition name="fade">
+        <div class="content-mask" v-show="ifShowContent" @click="hideContent">
+        </div>
+    </transition>
 </div>
 </template>
 
 <script>
+import ContentView from '@/components/Content'
 export default {
+    //对引入组件进行注册 
+    components: {
+        ContentView
+    },
     props: {
         ifTitleAndMenuShow: {
             type: Boolean,
@@ -71,19 +82,49 @@ export default {
         defaultFontSize: Number,
         themeList: Array,
         defaultTheme: Number,
-        bookAvailable: Boolean
+        bookAvailable: {
+            type: Boolean,
+            default: false
+        },
+        navigation: Object,
+        bookProgress: Number
     },
     data() {
         return {
             ifSettingShow: false,
             showTag: 0,
-            progress: 0
+            progress: 0,
+            ifShowContent: false
         }
     },
-    methods: {
-        onProgressInput(progress) {
-            this.progress = progress
+    watch: {
+        // 监听bookProgress的改变，动态赋值给progress
+        bookProgress : function (){
+            this.progress = this.bookProgress
         },
+    },
+    methods: {
+        returnISCT () {
+            return this.ifShowContent
+        },
+        showNav() {
+            this.ifShowContent = !this.ifShowContent,
+                this.ifSettingShow = false
+        },
+        //隐藏目录
+        hideContent() {
+            this.ifShowContent = false
+        },
+        // 调用父组件的跳转目录
+        jumpTo(target) {
+            this.$emit('jumpTo', target)
+        },
+        // 拖动进度条改变
+        // onProgressInput(progress) {
+        //     this.progress = progress
+        //    // this.$refs.progress.style.backgroundSize = `${this.progress}% 100%` //注意这里的是``而不是单引号，这是ES6的模板字符串
+        // },
+        // 拖动进度条后松开触发事件，跳转到指定位置
         onProgressChange(progress) {
             this.$emit('onProgressChange', progress)
         },
@@ -94,8 +135,12 @@ export default {
             this.$emit('setFontSize', fontSize)
         },
         showSetting(tag) {
-            this.ifSettingShow = true,
-                this.showTag = tag
+            if (this.showTag === tag) {
+                this.ifSettingShow = !this.ifSettingShow
+            } else {
+                this.ifSettingShow = true,
+                    this.showTag = tag
+            }
         },
         hideSetting() {
             this.ifSettingShow = false
@@ -108,6 +153,7 @@ export default {
 @import '../assets/styles/global';
 
 .menu-bar {
+
     .menu-wrapper {
         &.hide-box-shadow {
             box-shadow: none
@@ -120,7 +166,7 @@ export default {
         display: flex;
         width: 100%;
         height: px2rem(48);
-        background: wheat;
+        background: white;
         box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
 
         .icon-wrapper {
@@ -144,7 +190,7 @@ export default {
         width: 100%;
         height: px2rem(60);
         z-index: 101;
-        background: rgb(252, 251, 251);
+        background: white;
         box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
 
         .setting-font-size {
@@ -258,17 +304,59 @@ export default {
             position: relative;
             width: 100%;
             height: 100%;
-            .progress-wrapper {            
-            height: 100%;
-            width: 100;
-            padding: 0 px2rem(30);
-            box-sizing: border-box;
-            @include center;
-            .progress {}
+
+            .progress-wrapper {
+                height: 80%;
+                width: 100%;
+                padding: 0 px2rem(30);
+                box-sizing: border-box;
+                @include center;
+
+                .progress {
+                    width: 100%;
+                    -webkit-appearance: none;
+                    height: px2rem(2);
+                    border-radius: 10%;
+                    background: -webkit-linear-gradient(#999, #999) no-repeat, #ddd;
+                    background-size: 0 100%;
+
+                    &:focus {
+                        outline: none;
+                    }
+
+                    &::-webkit-slider-thumb {
+                        -webkit-appearance: none;
+                        width: px2rem(20);
+                        height: px2rem(20);
+                        border-radius: 50%;
+                        background: white;
+                        box-shadow: 0 px2rem(4) px2rem(4) 0 rgba(0, 0, 0, .15);
+                        border: px2rem(1) solid #ddd;
+                    }
+                }
+
             }
 
-            .text-wrapper {}
+            .text-wrapper {
+                position: absolute;
+                bottom: px2rem(1);
+                width: 100%;
+                color: #666;
+                font-size: px2rem(12);
+                @include center;
+            }
         }
+    }
+
+    .content-mask {
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 101;
+        display: flex;
+        width: 100%;
+        height: 100%;
+        background: rgba(51, 51, 51, .8);
     }
 }
 </style>
